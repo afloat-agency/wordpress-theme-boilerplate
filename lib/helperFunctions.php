63757bb5626b70c26f555d4880ffcf60514f3b6f
Sam Mckay <sammckay1105@gmail.com>
2018-02-25 17:51:26 +0000

    Made PSR2

--- lib/helperFunctions.php
+++ lib/helperFunctions.php
@@ -1,18 +1,18 @@
 <?php
 
-
 /**
  * Dump a variable in nice <pre> tags then die
  */
-function dd($var) {
-  echo '<pre>';
+function dd($var)
+{
+    echo '<pre>';
 
-  if (!isset($var)) {
-    echo 'No value passed to dd function.';
-  }
+    if (!isset($var)) {
+        echo 'No value passed to dd function.';
+    }
 
-  var_dump($var);
+    var_dump($var);
 
-  echo '</pre>';
-  die();
+    echo '</pre>';
+    die();
 }
